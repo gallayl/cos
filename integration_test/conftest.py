a48086ec9d60f2f89881cf2a3fda0eefa482faac
Gallay Lajos <gallay.lajos@gmail.com>
2026-02-18 10:01:56 +0100

    ci fix

--- integration_test/conftest.py
+++ integration_test/conftest.py
@@ -1,30 +1,36 @@
 import subprocess
 import sys
 
-if sys.platform == "win32":
-    _OrigPopen = subprocess.Popen
+_OrigPopen = subprocess.Popen
 
-    class _WinPopen(_OrigPopen):
-        """
-        Work around two Windows/ESP-IDF incompatibilities with pytest-embedded-qemu:
-        1. 'esptool.py' is not a valid Windows executable name -> use 'esptool'
-        2. Old esptool uses 'merge_bin' (underscore) but plugin sends 'merge-bin' (hyphen)
-        """
 
-        @staticmethod
-        def _fix_args(args):
-            if isinstance(args, (list, tuple)):
-                args = list(args)
-                if args and args[0] == "esptool.py":
-                    args[0] = "esptool"
-                args = [a.replace("merge-bin", "merge_bin") if a == "merge-bin" else a for a in args]
-            elif isinstance(args, str):
-                if args.startswith("esptool.py "):
-                    args = "esptool " + args[len("esptool.py "):]
-                args = args.replace(" merge-bin ", " merge_bin ")
-            return args
+class _PatchedPopen(_OrigPopen):
+    """
+    Work around incompatibilities between pytest-embedded-qemu and ESP-IDF's
+    bundled esptool:
 
-        def __init__(self, args, *a, **kw):
-            super().__init__(self._fix_args(args), *a, **kw)
+    - (All platforms) esptool bundled with ESP-IDF v5.5.x uses 'merge_bin'
+      (underscore) but pytest-embedded-qemu sends 'merge-bin' (hyphen).
+    - (Windows only) 'esptool.py' is not a valid executable name on Windows
+      because CreateProcess cannot resolve .py files. Rewrite to 'esptool'
+      so Windows finds esptool.exe.
+    """
 
-    subprocess.Popen = _WinPopen
+    @staticmethod
+    def _fix_args(args):
+        if isinstance(args, (list, tuple)):
+            args = list(args)
+            if sys.platform == "win32" and args and args[0] == "esptool.py":
+                args[0] = "esptool"
+            args = ["merge_bin" if a == "merge-bin" else a for a in args]
+        elif isinstance(args, str):
+            if sys.platform == "win32" and args.startswith("esptool.py "):
+                args = "esptool " + args[len("esptool.py "):]
+            args = args.replace(" merge-bin ", " merge_bin ")
+        return args
+
+    def __init__(self, args, *a, **kw):
+        super().__init__(self._fix_args(args), *a, **kw)
+
+
+subprocess.Popen = _PatchedPopen
